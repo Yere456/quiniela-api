d5f08eb14601430cff1bd1daea77a281c3bf2b8e
Yere456 <yeremybonilla@gmail.com>
2022-11-08 11:42:21 -0400

    input imagen

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express()
 
 app.use(cors())
 app.use(morgan('dev'))
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
 
 app.use('/', equiposRoutes)
 app.use('/', partidosRoutes)
